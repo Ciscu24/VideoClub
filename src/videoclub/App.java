c4e009dd55dfab32f00976b378d0ded4148563df
ralba <ralba@DESKTOP-761HVSP>
2020-02-24 14:04:20 +0100

    commit2

--- src/videoclub/App.java
+++ src/videoclub/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         GUI.principal();
         System.out.println("hola esto es una prueba");
+        System.out.println("puta");
     }
 }
