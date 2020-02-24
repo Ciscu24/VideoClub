59a46ee03e67e7876b5a0ea5edec005841eba2cc
ralba <ralba@DESKTOP-761HVSP>
2020-02-24 14:05:44 +0100

    empezamos

--- src/videoclub/App.java
+++ src/videoclub/App.java
@@ -3,7 +3,5 @@ package videoclub;
 public class App {
     public static void main(String[] args) {
         GUI.principal();
-        System.out.println("hola esto es una prueba");
-        System.out.println("puta");
     }
 }
